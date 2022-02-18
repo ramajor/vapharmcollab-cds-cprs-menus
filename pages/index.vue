9cb08a0824498358aae3cb6b5ab165da89739690
Ronnie Major <ramajor@gmail.com>
2022-02-17 23:37:25 -0500

    restore from backup because stackblitz...

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <Tutorial/>
+  <div>
+    <p>test home page</p>
+    <NuxtLink to="PrimaryMenu">Go to Primary Menus Test</NuxtLink>
+  </div>
 </template>
+<script>
+export default {
+  computed: {},
+};
+</script>
